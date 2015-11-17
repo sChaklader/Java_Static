4dd29c8dc38cbd373d3d89f41dae6a211b1a4b54
sChaklader <shchaklader@gmail.com>
2015-11-17 16:43:42 -0500

    Java Static variable

---
+++ JavaSatic/src/com/java/Student.java
@@ -0,0 +1,30 @@
+package com.java;
+
+public class Student {
+	
+		   int sRollno;  
+		   String name;  
+		   static String school ="NYU";  
+		     
+		   Student(int r,String n){ 
+		 
+		   sRollno = r;  
+		   name = n;  
+
+		   }  
+
+		 void display (){
+			System.out.println(sRollno+" "+name+" "+school);
+			}  
+		  
+		 public static void main(String args[]){ 
+		 
+		 Student sR1 = new Student(555,"Robat");  
+		 Student sR2 = new Student(786,"Salman");  
+		   
+		 sR1.display();  
+		 sR2.display();  
+
+		 }  
+
+		}  
